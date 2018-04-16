56049825b0b20d9a5a6d2edac32f972c3085a6ac
shuest <zhanshua@163.com>
2018-04-16 08:51:14 +0800

    2

--- 88.cpp
+++ 88.cpp
@@ -8,9 +8,39 @@
 #include<set>
 #include<map>
 using namespace std;
+
+//倒着相加，从后往前看
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        
+        int len=m+n-1;
+		m--;n--;
+		while(len>=0)
+		{
+			if(m>=0&&n>=0)
+			{
+				if(nums1[m]>nums2[n])
+				{
+					nums1[len]=nums1[m];
+					m--;
+				}
+				else
+				{
+					nums1[len]=nums2[n];
+					n--;
+				}
+				len--;
+			}
+			else if(m>=0)
+			{
+				nums1[len]=nums1[m];
+				len--;m--;
+			}
+			else if(n>=0)
+			{
+				nums1[len]=nums2[n];
+				len--;n--;
+			}
+		}
     }
 };
