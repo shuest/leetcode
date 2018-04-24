530f24e273f56758dc74a7db4d95bb798ee2c737
shuest <zhanshua@163.com>
2018-04-24 23:35:56 +0800

    4

---
+++ 203.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<cstdio>
+#include<cmath>
+#include<string>
+#include<cstring>
+#include<algorithm>
+#include<vector>
+#include<set>
+#include<map>
+
+using namespace std;
+
+
+
+  struct ListNode {
+      int val;
+     ListNode *next;
+      ListNode(int x) : val(x), next(NULL) {}
+ };
+ 
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        
+    }
+};
